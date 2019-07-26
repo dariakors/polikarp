780ca131bc4297978b83ce52a72db38bfc8207bc
Daria Korsakova <dashirak.kuznetsova@gmail.com>
2019-07-27 01:42:12 +0700

    change-tests-according-to-testing-web-service-get-method

--- tests/test_number.py
+++ tests/test_number.py
@@ -6,46 +6,46 @@ from parameterized import parameterized
 class Tests(unittest.TestCase):
 
     @parameterized.expand([
-        [2, True],
-        [3, False],
-        ['2', True],
-        ['3', False]
+        ['?number=1', False],
+        ['?number=2', True],
+        ['?number=-1', False],
+        ['?number=-2', True]
     ])
-    def test_valid_data(self, number, result):
-        response = requests.get('http://localhost/is_even', params={'number': number})
+    def test_valid_data(self, query_string, result):
+        response = requests.get('http://localhost/is_even' + query_string)
         self.assertEqual(response.json(), result)
 
     @parameterized.expand([
-        ["[1]"],
-        ["[1,2]"],
-        ["{1}"],
-        ["{1: 2}"],
-        ["a"],
-        [".4e7"],
-        ["2.0"],
-        ["[]"],
-        ["{}"],
-        [""],
+        ['?number=[]'],
+        ["?number=[1]"],
+        ["?number=[1,2]"],
+        ['?number={}'],
+        ["?number={1}"],
+        ["?number={1: 2}"],
+        ['?number=()'],
+        ["?number=(1, 2)"],
+        ["?number"],
+        ["?number="],
+        ["?number=''"],
+        ["?number=a"],
+        ["?number=.4e7"],
+        ["?number=2.0"],
+        ['?number="2"'],
+        ['?number=True'],
+        ['?number=None']
     ])
-    def test_valid_not_int_number(self, number):
-        response = requests.get('http://localhost/is_even', params={'number': number})
+    def test_not_int_number(self, query_string):
+        response = requests.get('http://localhost/is_even' + query_string)
         self.assertEqual(415, response.status_code)
+        self.assertEqual('Number must be integer', response.json()['error'])
 
-    @parameterized.expand([
-        [{}],
-        [[]]
-    ])
-    def test_missed_number(self, number):
-        response = requests.get('http://localhost/is_even', params={'number': number})
+    def test_missed_number(self):
+        response = requests.get('http://localhost/is_even?numb')
         self.assertEqual(400, response.status_code)
         self.assertEqual("You've missed the \"number\" parameter", response.json()['error'])
 
-    @parameterized.expand([
-        [[1, 2]],
-        [{1: 2, 2: 3}]
-    ])
-    def test_invalid_not_int_number(self, number):
-        response = requests.get('http://localhost/is_even', params={'number': number})
+    def test_invalid_not_int_number(self):
+        response = requests.get('http://localhost/is_even?number=1&number=2')
         self.assertEqual(400, response.status_code)
         self.assertEqual('Too much numbers', response.json()['error'])
 
