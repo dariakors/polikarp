2444270bb7b80aa8c2c9ca2135a1d81d85837fbf
Daria Korsakova <dashirak.kuznetsova@gmail.com>
2019-07-26 09:19:16 +0700

    add-400-and-415-errors-handlers

--- service/service.py
+++ service/service.py
@@ -4,19 +4,36 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+def is_int(number):
+    try:
+        int(number)
+    except ValueError:
+        return False
+
+    return True
+
+
 @app.route("/")
 def hello():
-    return "Hello!"
+    return "Hello, I'm Polikarp! Let's help you to understand whether number is even or not"
 
 
 @app.route("/is_even", methods=["GET"])
 def is_even():
-    number = request.args.get('number')
+    number = request.args.getlist('number')
+    print(number)
 
-    if int(number) % 2 == 0:
-        return jsonify(True)
+    if len(number) > 1:
+        return jsonify({"error": "Too much numbers"}), 400
 
-    return jsonify(False)
+    if is_int(number[0]):
+
+        if int(number[0]) % 2 == 0:
+            return jsonify(True)
+
+        return jsonify(False)
+
+    return jsonify({"error": "Number must be integer"}), 415
 
 
 if __name__ == "__main__":
