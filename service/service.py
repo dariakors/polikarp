588cb8b04289b36491c9bb8e9807c9274ef0b89f
Daria Korsakova <dashirak.kuznetsova@gmail.com>
2019-07-27 19:16:02 +0700

    description-for-flasgger-was-added

--- service/service.py
+++ service/service.py
@@ -1,7 +1,9 @@
+from flasgger import Swagger
 from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 
 def is_int(number):
@@ -20,6 +22,20 @@ def hello():
 
 @app.route("/is_even", methods=["GET"])
 def is_even():
+    """Endpoint returns whether number is even or not
+        ---
+        parameters:
+          - name: number
+            in: query
+            required: true
+        responses:
+            200:
+                description: true or false is returned
+            415:
+                description: number must be integer
+            400:
+                description: you've missed the 'number' parameter or too much numbers are gotten
+    """
     number = request.args.getlist('number')
 
     if len(number) > 1:
@@ -37,7 +53,7 @@ def is_even():
         return jsonify({"error": "Number must be integer"}), 415
 
     except IndexError:
-        return jsonify({"error": "You've missed the \"number\" parameter"}), 400
+        return jsonify({"error": "You've missed the 'number' parameter"}), 400
 
 
 if __name__ == "__main__":
